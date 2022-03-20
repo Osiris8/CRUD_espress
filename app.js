aacb0a2c55b86e7c8f6425b18a76159dc0f02ed7
Osiris8 <42705327+Osiris8@users.noreply.github.com>
2022-03-20 15:03:56 +0100

    Completed Project

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+
+const stuffRoutes = require("./routes/stuff");
+
+const userRoutes = require("./routes/user");
+
+const path = require("path");
+
+app.use(express.json());
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://osiris:osiris@cluster0.z3a2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+module.exports = app;
